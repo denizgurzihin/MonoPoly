44c4545eaece186d2e71cd11a63e85e9107717e7
Mustafa A. Hakkoz <mustafahakkoz@marun.edu.tr>
2018-12-06 23:42:29 +0200

    added new classes and new functionalities for iteration-2. some classes have blank methods which are marked with comments, to fulfill later.

---
+++ GoToJailSquare.java
@@ -0,0 +1,13 @@
+public class GoToJailSquare extends Square {
+    private JailSquare jail;
+
+    GoToJailSquare(String name, int location, JailSquare jail){ //Jailsiz şekliyle bi dene
+        super(name, location);
+        this.jail=jail;
+    }
+
+    @Override
+    public void landedOn(Player player) {
+        player.getPawn().setLocation(jail);
+    }
+}
